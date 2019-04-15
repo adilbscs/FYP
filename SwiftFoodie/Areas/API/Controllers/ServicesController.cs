912e23223a6c93dcac0144b23651916d38a15a75
Haris Khurshid <hariskhurshid@mcpinsight.com>
2019-04-15 05:13:08 +0500

    api method added

--- SwiftFoodie/Areas/API/Controllers/ServicesController.cs
+++ SwiftFoodie/Areas/API/Controllers/ServicesController.cs
@@ -46,5 +46,21 @@ namespace SwiftFoodie.Areas.API.Controllers
                 return Json(new { msg = "exception", response = ex.Message }, JsonRequestBehavior.AllowGet);
             }
         }
+        public JsonResult RiderLogin(Users user)
+        {
+            try
+            {
+                var users = db.Users.Where(u => u.UserName == user.UserName && u.Password == user.Password && u.IsActive == true && u.Role == 3).FirstOrDefault();
+                if (users != null)
+                {
+                    return Json(new { msg = "found", response = users }, JsonRequestBehavior.AllowGet);
+                }
+                return Json(new { msg = "notfound" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { msg = "exception", response = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
     }
 }
